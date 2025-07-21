88c7e7c4b9eef1972fa3d1dbe55f56e0d7662722
Abhigyan1112 <abhigyansharma.hhh@gmail.com>
2025-07-21 11:09:32 +0530

    hello

--- TextUtils/src/Components/Alert.js
+++ TextUtils/src/Components/Alert.js
@@ -2,7 +2,7 @@
 export default function Alert(props) {
   return (
     <div style={{height:'10vh'}}>
-    {props.alert && <div class={`alert alert-${props.alert.type}`} role="alert">
+    {props.alert && <div className={`alert alert-${props.alert.type}`} role="alert">
         <strong>{props.alert.type}</strong> : {props.alert.msg}
     </div>}
     </div>
